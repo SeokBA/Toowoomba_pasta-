552ab229946f89d8150f91a2a209605e21eb3895
SeokBA <cpswo123@gmail.com>
2019-11-21 02:04:35 +0900

    update RouterDlg

--- CptNetwork/src/router/ProxyARPTable.java
+++ CptNetwork/src/router/ProxyARPTable.java
@@ -7,34 +7,33 @@ public class ProxyARPTable {
     // key : hostID
     // value : _ProxyArpData -> host ip address, router Mac address
     private static Map<String, ProxyARPRecord> proxyArpTable = new HashMap<>();
-    private ProxyARPTable(){}
 
-    private static class _Holder{
+    private static class _Holder {
         // instance 최초 1회 할당.
-        private static final ProxyARPTable instance=new ProxyARPTable();
+        private static final ProxyARPTable instance = new ProxyARPTable();
     }
 
-    public static ProxyARPTable getInstance(){
+    public static ProxyARPTable getInstance() {
         return _Holder.instance;
     }
 
-    public Map<String, ProxyARPRecord> getProxyArpEntry(){
+    public Map<String, ProxyARPRecord> getProxyArpEntry() {
         return proxyArpTable;
     }
 
-    public boolean isInProxyArpEntry(String hostIpAddr){
+    public boolean isInProxyArpEntry(String hostIpAddr) {
         boolean state;
-        for(Map.Entry<String, ProxyARPRecord> entry : proxyArpTable.entrySet()){
-            if(entry.getValue().hostIpAddr.equals(hostIpAddr)){
+        for (Map.Entry<String, ProxyARPRecord> entry : proxyArpTable.entrySet()) {
+            if (entry.getValue().hostIpAddr.equals(hostIpAddr)) {
                 return true;
             }
         }
         return false;
     }
 
-    public String getMacAddr(String hostID){
-        for(Map.Entry<String, ProxyARPRecord> entry : proxyArpTable.entrySet()){
-            if(entry.getValue().hostIpAddr.equals(hostID)){
+    public String getMacAddr(String hostID) {
+        for (Map.Entry<String, ProxyARPRecord> entry : proxyArpTable.entrySet()) {
+            if (entry.getValue().hostIpAddr.equals(hostID)) {
                 return entry.getValue().routerMacAddr;
             }
         }
--- CptNetwork/src/router/RouterDlg.java
+++ CptNetwork/src/router/RouterDlg.java
@@ -22,7 +22,7 @@ public class RouterDlg extends JFrame implements BaseLayer {
     public int nUnderLayerCount = 0;
     public String pLayerName = null;
     public BaseLayer p_UnderLayer = null;
-    public ArrayList<BaseLayer> p_aUpperLayer = new ArrayList<>();
+    public ArrayList<BaseLayer> p_aUpperLayer = new ArrayList<BaseLayer>();
     public ArrayList<BaseLayer> p_aUnderLayer = new ArrayList<>();
     BaseLayer UnderLayer;
 
